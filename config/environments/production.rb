68e1ee058c0c9387c34cfbe26e53690249eb8fc1
Alex Melcon <amelc001@fiu.edu>
2016-04-19 01:04:19 -0400

    updated sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app49541889@heroku.com'],
-    :password       => ENV['dp5vkpwm3423'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
